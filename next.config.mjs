1173a1e4e5e311233f953334215219501dfa6b3a
manicdon7 <manicdon7@gmail.com>
2024-11-06 14:59:14 +0530

    config for hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
+  },
+
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./build", // Changes the build output directory to `./dist`.
   images: {
