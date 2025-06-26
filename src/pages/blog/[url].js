9c384c57017d9509f0895234af90c9f9558af80f
krnkiran22 <kirandev2210@gmail.com>
2025-06-26 19:20:09 +0530

    testing

--- src/pages/blog/[url].js
+++ src/pages/blog/[url].js
@@ -115,62 +115,15 @@ export const fetchBlogs = async (setBlogsData) => {
   return data;
 };
 
-export default function BlogPost({ blog }) {
+export default function BlogPost() {
   const router = useRouter();
+
   var { url } = router.query;
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  // Use the blog prop for initial render
-  const [blogDetails, setBlogDetails] = useState(
-    blog
-      ? {
-          title: blog.heading,
-          preview:
-            blog.content
-              .replaceAll("[", " ")
-              .replaceAll("]", " ")
-              .replaceAll("/n", " ")
-              .replaceAll("\n", " ")
-              .replaceAll("/", " ")
-              .replaceAll("*", " ")
-              .slice(0, 300) + "...",
-          image: blog.image,
-          tags: blog.tags,
-          Date: new Date(blog.date).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          }),
-          Updated: new Date(blog.modifiedon).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          }),
-          Publisher: {
-            name: blog.author
-              ? authorsData.find((e) => e.to === blog.author)?.name
-              : "SecureDapp",
-            image: blog.author
-              ? authorsData.find((e) => e.to === blog.author)?.image
-              : "",
-          },
-          author: blog.author,
-          Index: extractHeadings(blog.content),
-          Summary:
-            blog.content
-              .replaceAll("[", " ")
-              .replaceAll("]", " ")
-              .replaceAll("/n", " ")
-              .replaceAll("\n", " ")
-              .replaceAll("/", " ")
-              .replaceAll("*", " ")
-              .slice(0, 300) + "...",
-          Content: renderContent(blog),
-        }
-      : undefined
-  );
+  const [blogDetails, setBlogDetails] = useState();
   const [blogsData, setBlogsData] = useState();
   const [shortIndexView, setShortIndexView] = useState(true);
   const [relatedArticles, setRelated] = useState([]);
